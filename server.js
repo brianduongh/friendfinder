4ed62c8afb631dfe037e5f7ddf4bdc31ae13cdeb
Brian Duong <brianduongh@gmail.com>
2018-12-04 23:53:37 -0600

    added port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // Express & PORT
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
